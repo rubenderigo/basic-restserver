10552aabebcc6b28d1f353ca2e8001c6fe94631b
Ruben Derigo <68236987+rubenderigo@users.noreply.github.com>
2024-03-16 01:04:08 -0300

    Update server.js

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 8080;
     this.usuariosPath = "/api/usuario";
 
     // middlewares
